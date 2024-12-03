b87cb0dd2c8ced4710c85131130f531f7f679be8
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2024-12-02 19:55:53 -0500

    Day 2 Part 2.

--- Day02/main.cpp
+++ Day02/main.cpp
@@ -61,16 +61,138 @@ auto Part1(const std::vector<std::string>& input)
 	return safeCount;
 }
 
+// Returns the index of the first number that is not safe. If given 1 2 3 and 2 compared to 3 is not safe, it will return the index of 2.
+// If all numbers are safe, it will return -1.
+int FindUnsafeIndex(const std::vector<int>& numbers)
+{
+	int numberNew;
+	int numberOld;
+	bool isIncrease = false;
+	bool isDecrease = false;
+	bool isSafe = true;
+
+	int index = 0;
+	numberOld = numbers[index];
+	for (index = 1; index < numbers.size(); index++)
+	{
+		numberNew = numbers[index];
+		if (numberOld < numberNew)
+		{
+			isIncrease = true;
+		}
+		else if (numberOld > numberNew)
+		{
+			isDecrease = true;
+		}
+
+		// At least 1, at most 3
+		if (!(abs(numberNew - numberOld) >= 1) || !(abs(numberNew - numberOld) <= 3))
+		{
+			std::cout << "Unsafe, range\n";
+			isSafe = false;
+			break;
+		}
+
+		// Can't increase and decrease
+		if (isIncrease && isDecrease)
+		{
+			std::cout << "Unsafe, increase & decrease\n";
+			isSafe = false;
+			break;
+		}
+
+		numberOld = numberNew;
+	}
+
+	if (!isSafe)
+	{
+		return index - 1;
+	}
+	return -1;
+}
+
 auto Part2(const std::vector<std::string>& input)
 {
 	Utils::Timer timer("Part 2");
 
+	int safeCount = 0;
 	for (const auto& line : input)
 	{
-		std::cout << line << '\n';
+		std::cout << "Line: " << line << '\n';
+
+		std::istringstream inputStringStream(line);
+		std::vector<int> numbers;
+
+		while (!inputStringStream.eof())
+		{
+			int number;
+			inputStringStream >> number;
+			numbers.push_back(number);
+		}
+
+		int unsafeIndex = FindUnsafeIndex(numbers);
+		if (unsafeIndex == -1)
+		{
+			std::cout << "Safe\n";
+			safeCount++;
+		}
+		else // I'm not very proud of this mess...
+		{
+			std::cout << "Unsafe detected at index " << unsafeIndex << ". Trying to remove the unsafe number and check again.\n";
+			std::vector<int> numbersCopy = numbers;
+			std::cout << "Correction Attempt 1: (Remove index)\n";
+			numbersCopy.erase(numbersCopy.begin() + unsafeIndex);
+			for (int i = 0; i < numbersCopy.size(); i++)
+			{
+				std::cout << numbersCopy[i] << ' ';
+			}
+			std::cout << '\n';
+			if (FindUnsafeIndex(numbersCopy) == -1)
+			{
+				std::cout << "Safe\n";
+				safeCount++;
+				continue; // Ignore the rest, continue to the next input line.
+			}
+
+			numbersCopy = numbers;
+			std::cout << "Correction Attempt 2: (Remove index + 1)\n";
+			numbersCopy.erase(numbersCopy.begin() + unsafeIndex + 1);
+			for (int i = 0; i < numbersCopy.size(); i++)
+			{
+				std::cout << numbersCopy[i] << ' ';
+			}
+			std::cout << '\n';
+			if (FindUnsafeIndex(numbersCopy) == -1)
+			{
+				std::cout << "Safe\n";
+				safeCount++;
+				continue; // Ignore the rest, continue to the next input line.
+			}
+
+			// If the unsafeIndex is at 1, perhaps it is index 0 that is the problem?
+			if (unsafeIndex == 1)
+			{
+				numbersCopy = numbers;
+				std::cout << "Correction Attempt 3: (Remove first)\n";
+				numbersCopy.erase(numbersCopy.begin());
+				for (int i = 0; i < numbersCopy.size(); i++)
+				{
+					std::cout << numbersCopy[i] << ' ';
+				}
+				std::cout << '\n';
+				if (FindUnsafeIndex(numbersCopy) == -1)
+				{
+					std::cout << "Safe\n";
+					safeCount++;
+					continue; // Ignore the rest, continue to the next input line.
+				}
+			}
+
+			std::cout << "No correction(s) found. Unsafe.\n";
+		}
 	}
 
-	return 0;
+	return safeCount;
 }
 
 int main(int argc, char** argv)
@@ -87,7 +209,7 @@ int main(int argc, char** argv)
 		Utils::CheckResult(result1, 2);
 
 		auto result2 = Part2(lines);
-		Utils::CheckResult(result2, 0);
+		Utils::CheckResult(result2, 4);
 	}
 	catch (const std::exception& e)
 	{
